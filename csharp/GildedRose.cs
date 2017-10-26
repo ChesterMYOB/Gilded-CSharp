94e5ac74483488e36ef515e417658862720a6c1d
Chester Booker <chester.booker@myob.com>
2017-10-27 09:32:59 +1300

    Extracted Sulfuras Tag

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -22,12 +22,78 @@ namespace csharp
 
         private static Item DoSomethingTo(Item item)
         {
-            if (item.Name != Tag.AgedBrie && item.Name != Tag.BackstagePass)
+            if (item.Name == Tag.Sulfuras)
             {
-                if (item.Quality > 0 && item.Name != Tag.Sulfuras)
+                ProcessItemsWithNegativeSellIn(item);
+            }
+            else
+            {
+                if (item.Name != Tag.AgedBrie && item.Name != Tag.BackstagePass)
                 {
-                    item.Quality = item.Quality - 1;
+                    if (item.Quality > 0)
+                    {
+                        item.Quality = item.Quality - 1;
+                    }
+                }
+                else
+                {
+                    if (item.Quality < 50)
+                    {
+                        ProcessItemWithUnder50Quality(item);
+                    }
+                }
 
+                item.SellIn = item.SellIn - 1;
+
+
+                if (item.SellIn < 0)
+                {
+                    ProcessItemsWithNegativeSellIn(item);
+                }
+            }
+
+
+
+            return item;
+        }
+
+        private static void ProcessSulfurasItem(Item item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private static void ProcessItemWithUnder50Quality(Item item)
+        {
+            item.Quality = item.Quality + 1;
+
+            if (item.Name == Tag.BackstagePass)
+            {
+                if (item.SellIn < 11 && item.Quality < 50)
+                {
+                    item.Quality = item.Quality + 1;
+                }
+
+                if (item.SellIn < 6 && item.Quality < 50)
+                {
+                    item.Quality = item.Quality + 1;
+                }
+            }
+        }
+
+        private static void ProcessItemsWithNegativeSellIn(Item item)
+        {
+            if (item.Name != Tag.AgedBrie)
+            {
+                if (item.Name != Tag.BackstagePass)
+                {
+                    if (item.Quality > 0 && item.Name != Tag.Sulfuras)
+                    {
+                        item.Quality = item.Quality - 1;
+                    }
+                }
+                else
+                {
+                    item.Quality = item.Quality - item.Quality;
                 }
             }
             else
@@ -35,54 +101,8 @@ namespace csharp
                 if (item.Quality < 50)
                 {
                     item.Quality = item.Quality + 1;
-
-                    if (item.Name == Tag.BackstagePass)
-                    {
-                        if (item.SellIn < 11 && item.Quality < 50)
-                        {
-                            item.Quality = item.Quality + 1;
-                        }
-
-                        if (item.SellIn < 6 && item.Quality < 50)
-                        {
-                            item.Quality = item.Quality + 1;
-                        }
-                    }
                 }
             }
-
-            if (item.Name != Tag.Sulfuras)
-            {
-                item.SellIn = item.SellIn - 1;
-            }
-
-            if (item.SellIn < 0)
-            {
-                if (item.Name != Tag.AgedBrie)
-                {
-                    if (item.Name != Tag.BackstagePass)
-                    {
-                        if (item.Quality > 0 && item.Name != Tag.Sulfuras)
-                        {
-
-                            item.Quality = item.Quality - 1;
-
-                        }
-                    }
-                    else
-                    {
-                        item.Quality = item.Quality - item.Quality;
-                    }
-                }
-                else
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality = item.Quality + 1;
-                    }
-                }
-            }
-            return item;
         }
     }
 }
